0f231aa0101cae70669fa492a1f354ad60294e5c
286799714 <41233300+286799714@users.noreply.github.com>
2021-02-19 14:10:58 +0800

    init

---
+++ src/main/java/cn/maydaymemory/blackmarket/BlackMarket.kt
@@ -0,0 +1,19 @@
+package cn.maydaymemory.blackmarket
+
+import io.izzel.taboolib.TabooLib
+import io.izzel.taboolib.loader.Plugin
+
+object BlackMarket : Plugin() {
+
+    override fun onLoad() {
+
+    }
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
